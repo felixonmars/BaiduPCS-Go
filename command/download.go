e777ba1761d42c4b542171deeaae04ef32cc21a4
apple <2571583272@qq.com>
2017-12-08 15:25:03 +0800

    递归解析通配符

--- command/download.go
+++ command/download.go
@@ -9,47 +9,53 @@ import (
 )
 
 // RunDownload 执行下载网盘内文件
-func RunDownload(path string) {
+func RunDownload(paths ...string) {
 	downloader.SetCacheSize(2048)
-	downloader.SetMaxParallel(thread)
+	downloader.SetMaxParallel(pcsconfig.Config.MaxParallel)
 
-	path, err := toAbsPath(path)
-	if err != nil {
-		fmt.Println(err)
-		return
+	var _paths []string
+	for k := range paths {
+		_paths = append(_paths, parsePath(paths[k])...)
 	}
 
-	downloadInfo, err := info.FilesDirectoriesMeta(path)
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println()
+	for k := range _paths {
+		fmt.Printf("添加下载任务: %s\n", _paths[k])
 	}
+	fmt.Println()
 
-	fmt.Println(downloadInfo)
+	for k, path := range _paths {
+		downloadInfo, err := info.FilesDirectoriesMeta(path)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-	// 如果是一个目录, 递归下载该目录下的所有文件
-	if downloadInfo.Isdir {
-		fmt.Printf("即将下载目录: %s\n\n", path)
+		fmt.Printf("[ %d / %d ] %s\n", k+1, len(_paths), downloadInfo.String())
 
-		fileN, directoryN, size := recurseFDCountTotalSize(path)
-		statText := fmt.Sprintf("统计: 目录总数: %d, 文件总数: %d, 文件总大小: %s\n\n",
-			directoryN,
-			fileN,
-			pcsutil.ConvertFileSize(size),
-		)
+		// 如果是一个目录, 递归下载该目录下的所有文件
+		if downloadInfo.Isdir {
+			fmt.Printf("即将下载目录: %s\n\n", path)
 
-		fmt.Printf(statText)
-		downloadDirectory(path)
-		fmt.Printf("目录 %s 下载完成, %s", path, statText)
-		return
-	}
+			fileN, directoryN, size := recurseFDCountTotalSize(path)
+			statText := fmt.Sprintf("统计: 目录总数: %d, 文件总数: %d, 文件总大小: %s\n\n",
+				directoryN,
+				fileN,
+				pcsutil.ConvertFileSize(size),
+			)
 
-	fmt.Printf("即将开始下载文件\n\n")
+			fmt.Printf(statText)
+			downloadDirectory(path)
+			fmt.Printf("目录 %s 下载完成, %s", path, statText)
+			continue
+		}
 
-	err = info.FileDownload(path, downloadInfo.Size)
-	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Printf("即将开始下载文件\n\n")
+
+		err = info.FileDownload(path, downloadInfo.Size)
+		if err != nil {
+			fmt.Printf("下载文件时发生错误: %s (跳过...)\n\n", err)
+		}
 	}
 }
 
