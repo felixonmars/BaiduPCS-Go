58cd77013297b41b95d36331569dfd713b1c11aa
apple <2571583272@qq.com>
2017-12-15 14:12:58 +0800

    获取本地储存路径, 由获取相对路径改为获取绝对路径

--- config/get_checker.go
+++ config/get_checker.go
@@ -6,8 +6,10 @@ import (
 	"github.com/iikira/BaiduPCS-Go/downloader"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+// GetBaiduUserByUID 通过 百度uid 获取 Baidu 指针对象
 func (c *PCSConfig) GetBaiduUserByUID(uid uint64) (*Baidu, error) {
 	for k := range c.BaiduUserList {
 		if uid == c.BaiduUserList[k].UID {
@@ -17,17 +19,20 @@ func (c *PCSConfig) GetBaiduUserByUID(uid uint64) (*Baidu, error) {
 	return nil, fmt.Errorf("未找到百度帐号")
 }
 
+// GetAllBaiduUser 返回所有已登录百度帐号
 func (c *PCSConfig) GetAllBaiduUser() string {
 	var s bytes.Buffer
 	s.WriteString("\nindex\t\tuid\t用户名\n")
+	s.WriteString(strings.Repeat("-", 50) + "\n")
 
 	for k := range c.BaiduUserList {
-		s.WriteString(fmt.Sprintf("%4d", k) + "\t" + fmt.Sprintf("%11d", c.BaiduUserList[k].UID) + "\t" + c.BaiduUserList[k].Name + "\n")
+		s.WriteString(fmt.Sprintf("%4d|", k) + "\t" + fmt.Sprintf("%11d|", c.BaiduUserList[k].UID) + "\t" + c.BaiduUserList[k].Name + "\n")
 	}
-	s.WriteString("\n")
+	s.WriteRune('\n')
 	return s.String()
 }
 
+// CheckUIDExist 检查 百度uid 是否存在于已登录列表
 func (c *PCSConfig) CheckUIDExist(uid uint64) bool {
 	for k := range c.BaiduUserList {
 		if uid == c.BaiduUserList[k].UID {
@@ -37,14 +42,21 @@ func (c *PCSConfig) CheckUIDExist(uid uint64) bool {
 	return false
 }
 
-// GetSavePath 根据提供的网盘文件路径 path, 返回本地储存路径
+// GetSavePath 根据提供的网盘文件路径 path, 返回本地储存路径,
+// 返回绝对路径, 获取绝对路径出错时才返回相对路径...
 func GetSavePath(path string) string {
-	return filepath.Dir(fmt.Sprintf("%s/%d_%s%s/..",
+	dirStr := fmt.Sprintf("%s/%d_%s%s/.",
 		SaveDir,
 		ActiveBaiduUser.UID,
 		ActiveBaiduUser.Name,
 		path,
-	))
+	)
+
+	dir, err := filepath.Abs("./" + dirStr)
+	if err != nil {
+		dir = filepath.Dir(dirStr)
+	}
+	return dir
 }
 
 // CheckFileExist 检查本地文件是否与网盘的文件重名
