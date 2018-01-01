dee1aad859dcb41528c4cd4ab4e5376cfdfa3b3e
apple <2571583272@qq.com>
2018-01-01 21:07:58 +0800

    遍历目录内文件

---
+++ util/file.go
@@ -0,0 +1,26 @@
+package pcsutil
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// WalkDir 获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
+func WalkDir(dirPth, suffix string) (files []string, err error) {
+	files = make([]string, 0, 30)
+	suffix = strings.ToUpper(suffix)                                                     //忽略后缀匹配的大小写
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() { // 忽略目录
+			return nil
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			files = append(files, filename)
+		}
+		return nil
+	})
+	return files, err
+}
