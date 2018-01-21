07d382282202d35321bf641ee82decd16bd329b2
konica <2571583272@qq.com>
2018-01-21 18:51:41 +0800

    seter方法

--- config/seter.go
+++ config/seter.go
@@ -2,6 +2,7 @@ package pcsconfig
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // SetBDUSS 设置百度 bduss 并保存
@@ -16,4 +17,43 @@ func (c *PCSConfig) SetBDUSS(bduss string) (username string, err error) {
 	c.BaiduUserList = append(c.BaiduUserList, b)
 	c.BaiduActiveUID = b.UID
 	return b.Name, c.Save()
+}
+
+func (c *PCSConfig) Set(key, value string) (err error) {
+	switch key {
+	case "cache_size", "max_parallel":
+		intVal, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Printf("%s 不合法, 错误: %s\n", key, err)
+			return err
+		}
+
+		if intVal <= 0 {
+			fmt.Printf("%s 不合法, 值应为一个正整数\n", key)
+			return nil
+		}
+
+		switch key {
+		case "cache_size":
+			c.CacheSize = intVal
+		case "max_parallel":
+			c.MaxParallel = intVal
+		}
+
+		err = c.Save()
+		if err != nil {
+			fmt.Println("设置失败, 错误:", err)
+			return nil
+		}
+		fmt.Printf("设置成功, %s -> %v\n", key, value)
+
+	case "savedir":
+		c.SaveDir = value
+		c.Save()
+		fmt.Printf("设置成功, %s -> %v\n", key, value)
+
+	default:
+		return fmt.Errorf("未知设定值: %s\n\n", key)
+	}
+	return nil
 }
