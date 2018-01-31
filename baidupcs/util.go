6124ab36021b86b021822b396bc813b1e1f95414
konica <2571583272@qq.com>
2018-01-31 23:31:46 +0800

    修改注释和错误信息

--- baidupcs/util.go
+++ baidupcs/util.go
@@ -8,7 +8,7 @@ import (
 func checkErr(json *simplejson.Json) (code int, msg error) {
 	codeJSON, ok1 := json.CheckGet("error_code")
 	msgJSON, ok2 := json.CheckGet("error_msg")
-	if !ok1 && !ok2 { // 没有错误
+	if !ok1 && !ok2 { // 未发现错误信息
 		return 0, nil
 	}
 
@@ -16,7 +16,7 @@ func checkErr(json *simplejson.Json) (code int, msg error) {
 	errMsg := msgJSON.MustString()
 	switch errCode {
 	case 31045: // user not exists
-		errMsg = "操作失败, 可能BDUSS已过期, 请尝试重新登录, 消息: " + errMsg
+		errMsg = "操作失败, 可能百度帐号登录状态过期, 请尝试重新登录, 消息: " + errMsg
 	}
 	return errCode, errors.New(errMsg)
 }
