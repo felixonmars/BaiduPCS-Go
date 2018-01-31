b7c4011fabc75b324c5a7c3cee0a94ab96172302
konica <2571583272@qq.com>
2018-01-31 23:27:12 +0800

    缓存支持

---
+++ pcscache/pcscache.go
@@ -0,0 +1,14 @@
+package pcscache
+
+import (
+	"github.com/iikira/BaiduPCS-Go/baidupcs"
+	"time"
+)
+
+var (
+	// DirCache 网盘目录缓存
+	DirCache = dirCache{
+		fdl:      map[string]*baidupcs.FileDirectoryList{},
+		lifeTime: 1 * time.Hour,
+	}
+)
