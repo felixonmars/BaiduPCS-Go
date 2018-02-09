0f7e913f90f25d5f59654c32d6985c843da408eb
konica <2571583272@qq.com>
2018-02-09 14:13:59 +0800

    修复手机号不能登录的问题

--- pcsutil/regexp_pre.go
+++ pcsutil/regexp_pre.go
@@ -8,5 +8,5 @@ var (
 	// HTTPSRE https regexp
 	HTTPSRE = regexp.MustCompile("^https")
 	// ChinaPhoneRE https regexp
-	ChinaPhoneRE = regexp.MustCompile("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$")
+	ChinaPhoneRE = regexp.MustCompile(`^(\+86)?1[3-9][0-9]\d{8}$`)
 )
