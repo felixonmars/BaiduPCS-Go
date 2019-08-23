df195c733e496a65a5f4c356ee39e1fcc999b37e
iikira <2571583272@qq.com>
2019-08-23 14:29:01 +0800

    update dependency

--- vendor/github.com/iikira/baidu-tools/randominfo/randominfo.go
+++ vendor/github.com/iikira/baidu-tools/randominfo/randominfo.go
@@ -11,8 +11,8 @@ import (
 	"time"
 )
 
-// RamdomNumber 返回[min, max]随机数字
-func RamdomNumber(min, max uint64) (v uint64) {
+// RandomNumber 返回[min, max]随机数字
+func RandomNumber(min, max uint64) (v uint64) {
 	if min > max {
 		min, max = max, min
 	}
@@ -20,22 +20,29 @@ func RamdomNumber(min, max uint64) (v uint64) {
 	return v%(max-min) + min
 }
 
-// RamdomBytes 随机字节数组
-func RamdomBytes(n int) []byte {
+// RandomBytes 随机字节数组
+func RandomBytes(n int) []byte {
 	b := make([]byte, n)
 	cryptorand.Read(b)
 	return b
 }
 
-// RamdomMD5String 随机md5字符串
-func RamdomMD5String() string {
+// RandomMD5String 随机md5字符串
+func RandomMD5String() string {
 	m := md5.New()
 	m.Write(RamdomBytes(4))
 	m.Write([]byte(strconv.FormatInt(time.Now().UnixNano(), 16)))
 	return hex.EncodeToString(m.Sum(nil))
 }
 
-// RamdomMD5UpperString 随机md5字符串, 大写
-func RamdomMD5UpperString() string {
+// RandomMD5UpperString 随机md5字符串, 大写
+func RandomMD5UpperString() string {
 	return strings.ToUpper(RamdomMD5String())
 }
+
+var (
+	RamdomNumber         = RandomNumber
+	RamdomBytes          = RandomBytes
+	RamdomMD5String      = RandomMD5String
+	RamdomMD5UpperString = RandomMD5String
+)
