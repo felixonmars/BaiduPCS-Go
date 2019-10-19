a2b8717d5a065b5391b894fa6b41b01e11041d3c
iikira <2571583272@qq.com>
2019-10-19 10:15:33 +0800

    fix #790, 修复windows下编译失败

--- pcsliner/clear_windows.go
+++ pcsliner/clear_windows.go
@@ -1,21 +1,56 @@
 package pcsliner
 
 import (
-	"github.com/peterh/liner"
-	_ "unsafe" // for go:linkname
+	"syscall"
+	"unsafe"
 )
 
-//go:linkname eraseScreen github.com/iikira/BaiduPCS-Go/vendor/github.com/peterh/liner.(*State).eraseScreen
-func eraseScreen(s *liner.State)
+const (
+	std_output_handle = uint32(-11 & 0xFFFFFFFF)
+)
+
+var (
+	kernel32 = syscall.NewLazyDLL("kernel32.dll")
+
+	procGetStdHandle               = kernel32.NewProc("GetStdHandle")
+	procSetConsoleCursorPosition   = kernel32.NewProc("SetConsoleCursorPosition")
+	procGetConsoleScreenBufferInfo = kernel32.NewProc("GetConsoleScreenBufferInfo")
+	procFillConsoleOutputCharacter = kernel32.NewProc("FillConsoleOutputCharacterW")
+)
+
+type (
+	coord struct {
+		x, y int16
+	}
+	smallRect struct {
+		left, top, right, bottom int16
+	}
+	consoleScreenBufferInfo struct {
+		dwSize              coord
+		dwCursorPosition    coord
+		wAttributes         int16
+		srWindow            smallRect
+		dwMaximumWindowSize coord
+	}
+)
 
 // ClearScreen 清空屏幕
 func (pl *PCSLiner) ClearScreen() {
-	eraseScreen(pl.State)
+	ClearScreen()
 }
 
 // ClearScreen 清空屏幕
 func ClearScreen() {
-	liner := NewLiner()
-	liner.ClearScreen()
-	liner.Close()
+	out, _, _ := procGetStdHandle.Call(uintptr(std_output_handle))
+	hOut := syscall.Handle(out)
+
+	var sbi consoleScreenBufferInfo
+	procGetConsoleScreenBufferInfo.Call(uintptr(hOut), uintptr(unsafe.Pointer(&sbi)))
+
+	var numWritten uint32
+	procFillConsoleOutputCharacter.Call(uintptr(hOut), uintptr(' '),
+		uintptr(sbi.dwSize.x)*uintptr(sbi.dwSize.y),
+		0,
+		uintptr(unsafe.Pointer(&numWritten)))
+	procSetConsoleCursorPosition.Call(uintptr(hOut), 0)
 }
